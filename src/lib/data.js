b8f7b4050e52fdca43a098d9915eef0bfe9a8440
sohan01fw <sohancwactfw@gmail.com>
2024-11-26 22:46:02 +0545

    add school project

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,7 +1,15 @@
 import Ghumfir from "../assets/Ghumfir.png";
 import cinedb from "../assets/cinedb.png";
 import club from "../assets/club.png";
+import school from "../assets/school.png";
 export const data = [
+  {
+    imgSrc: school,
+    title: "School site",
+    goal: "To learn about the fully dynamic school site built from groundup and provide the comprehensive user expereince and functionality helps to improve the school visibility online. ",
+    siteLink: "https://photos.app.goo.gl/G7mYV66ydmYVM1zC9",
+    codeLink: "https://github.com/makaluschool/makalu-site-project",
+  },
   {
     imgSrc: Ghumfir,
     title: "Ghumfir(Best Travelling Experience)",
@@ -23,5 +31,5 @@ export const data = [
     siteLink:
       "https://photos.google.com/share/AF1QipMO832iExh3ZJsiVspECf8vWUjRB7Bs2-8c__7jH-s13hnv97OTYFCMl01y38Yk-w/photo/AF1QipO-3g-vTrrfimJm94qCjqm6jOJAOmHu-W2_PHys?key=ZHltZXFhbThYUUNjdmdDV0RqSjY3NXIxQmx2T19B",
     codeLink: "https://github.com/sohan01fw/cinedb",
-  }
+  },
 ];
